1ebde47b91271c7e0b39ee328efa9b5b0bfb75d0
tangyy <tangyueyang@lztogether.com>
2017-12-29 16:42:18 +0800

    Mod hook函数文件目录修改

--- cloud.js
+++ cloud.js
@@ -4,7 +4,7 @@ const path = require('path');
 const HOOK_DIR = path.join(__dirname, 'hook');
 
 fs.statSync(HOOK_DIR).isDirectory() && fs.readdirSync(HOOK_DIR).forEach(dir => {
-  const HOOK_FILE = path.join(HOOK_DIR, dir);
+  const HOOK_FILE = path.join(HOOK_DIR, dir, `${dir}.hook.g.js`);
   if (fs.statSync(HOOK_FILE).isFile())
     require(HOOK_FILE)
 });
