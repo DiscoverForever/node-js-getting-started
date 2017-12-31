c41e2c196f3bd1dff26d0996f3d8c5d22e897548
tangyy <tangyueyang@lztogether.com>
2017-12-31 17:39:36 +0800

    Mod require前校验文件是否存在

--- cloud.js
+++ cloud.js
@@ -4,7 +4,8 @@ const path = require('path');
 const HOOK_DIR = path.join(__dirname, 'hook');
 
 fs.statSync(HOOK_DIR).isDirectory() && fs.readdirSync(HOOK_DIR).forEach(dir => {
-  const HOOK_FILE = path.join(HOOK_DIR, dir);
-  if (fs.statSync(HOOK_FILE).isFile())
-    require(HOOK_FILE)
+  const HOOK_FUN_FILE = path.join(HOOK_DIR, dir, `${dir}.hook.g.js`);
+  const CLOUD_FUN_FILE = path.join(HOOK_DIR, dir, `${dir}.cloud.g.js`);
+  if (fs.existsSync(HOOK_FUN_FILE) && fs.statSync(HOOK_FUN_FILE).isFile()) require(HOOK_FUN_FILE);
+  if (fs.existsSync(CLOUD_FUN_FILE) && fs.statSync(CLOUD_FUN_FILE).isFile()) require(CLOUD_FUN_FILE);
 });
